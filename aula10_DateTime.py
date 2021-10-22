8138662c68ca0d96219e530b013b1d464713d20e
Alladio Bonesso <Alladio@users.noreply.github.com>
2021-10-22 20:32:54 -0300

    DateTime and Delta

--- aula10_DateTime.py
+++ aula10_DateTime.py
@@ -1,4 +1,4 @@
-from datetime import date, time
+from datetime import date, time, datetime, timedelta
 
 
 def trabalhandoComDate():
@@ -8,7 +8,6 @@ def trabalhandoComDate():
     print(dataAtual.strftime('%d/%m/%Y'))
     print(dataAtual.strftime('%A/%B/%Y'))
 
-
     data_atual_str = dataAtual.strftime('%D')
 
     print(dataAtual)
@@ -16,7 +15,44 @@ def trabalhandoComDate():
 
 def trabalhandoComTime():
     horario = time(hour=15, minute=18, second=30)
+    horario_str = horario.strftime('%H:%M:%S')
     print(horario)
+    print(type(horario))
+    print(horario_str)
+    print(type(horario_str))
 
+def trabalhandoComDateTime():
+    dataAtual = datetime.now();
+    print(dataAtual)
+    print(type(dataAtual))
+    dataAtualStr = (dataAtual.strftime('%d/%m/%Y %H:%M:%S'))
+    print(dataAtualStr)
+    print(type(dataAtual))
+
+    print((dataAtual.day))
+    print(str)
+
+    tupla = ('Segunda','Terça','Quarta','Quinta','Sexta', 'Sábado','Domingo')
+    array = ['Segunda','Terça','Quarta','Quinta','Sexta', 'Sábado','Domingo']
+    chaves = {'Segunda','Terça','Quarta','Quinta','Sexta', 'Sábado','Domingo'}
+    print(tupla[dataAtual.weekday()])
+    print(dataAtual.weekday())
+
+    # print(chaves(6))
+    # print(array(6))
+
+    #STRING para TIME
+    dataString = '01/01/2020 12:20:19'
+    dataConvertida = datetime.strptime(dataString, '%d/%m/%Y %H:%M:%S')
+    print(dataConvertida)
+
+    #CONTAS com DATAS
+    print(dataConvertida)
+    novaData = dataConvertida - timedelta(days=365, hours=10, minutes=10,
+                                          seconds=9,milliseconds=15,microseconds=10,
+                                          weeks=-1)
+    print(novaData)
 if __name__ == '__main__':
-    trabalhandoComDate()
+    # trabalhandoComDate()
+    # trabalhandoComTime()
+    trabalhandoComDateTime()
