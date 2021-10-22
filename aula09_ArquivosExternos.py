5e4fffff60e96e7dcc3e2884abdfb95c68aee997
Alladio Bonesso <Alladio@users.noreply.github.com>
2021-10-21 23:00:14 -0300

    split

--- aula09_ArquivosExternos.py
+++ aula09_ArquivosExternos.py
@@ -1,26 +1,43 @@
 # 'w' -> Escreve em um arquivo OBS: Limpa o que já tem
 # 'a' -> Adiciona ao arquivo existente
-
-def escreverArquivo(texto):
-    arquivo = open('teste.txt','w')
+caminho = 'E:/Alladio/projetos/python/alunos.txt'
+def escreverArquivo(texto, caminhoArquivo):
+    diretorioArquivo = caminhoArquivo
+    arquivo = open(diretorioArquivo,'w')
     arquivo.write(texto)
     arquivo.close()
 
-def adicionarArquivo(texto):
-    arquivo = open('teste.txt','a')
-    arquivo.write('\n')
+def adicionarArquivo(texto,caminhoArquivo):
+    arquivo = open(caminhoArquivo,'a')
+    # arquivo.write('\n')
     arquivo.write(texto)
     arquivo.close()
 
 
-def lerArquivo(nomeArquivo):
-    arquivo = open(nomeArquivo)
+def lerArquivo(caminhoArquivo):
+    arquivo = open(caminhoArquivo)
     texto = arquivo.read()
-    print(texto)
+    # print(texto)
 
+def mediaNotas(caminhoArquivo):
+    arquivo = open(caminhoArquivo)
+    alunoNota = arquivo.read()
+    # print(alunoNota)
+
+    alunoNotaSplit = alunoNota.split('\n')
+    # print(alunoNota)
+    for x in alunoNota:
+        alunoNotaSplit2 = x.split(',')
+        # aluno =x[0]
+        print(alunoNotaSplit2)
 
 if __name__ == '__main__':
+    caminhoArquivo=caminho
     # escreverArquivo('Primeira Escrita\n')
     # adicionarArquivo('Segunda Escrita\n')
     # adicionarArquivo('Quarta Escrita\n')
-    lerArquivo('teste.txt')
+    # escreverArquivo('Alladio, 10,9,10,9\n',caminhoArquivo)
+    # adicionarArquivo('Valentim, 9,9,9,9\n',caminhoArquivo)
+    # adicionarArquivo('Octavio, 8,8,8,8\n',caminhoArquivo)
+    # lerArquivo(caminhoArquivo)
+    mediaNotas(caminhoArquivo)
