c579af9e679060b64f48d2a9239e38dfb73c944e
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-02-28 19:13:44 -0300

    Add: Arrays(Matrizes)

---
+++ teste_and_Questions/test01/04_Arrays(Matrizes).py
@@ -0,0 +1,29 @@
+#Lista
+
+#Criando, Adicionando, Imprimindo
+lista = []
+lista.append(1)
+lista.append('Alladio')
+lista.append(3.14)
+print(lista)
+
+#remove ultimo elemento
+lista.pop()
+print(lista)
+
+#colocando ultimo elemento retirado
+lista.append(3.14)
+print(lista)
+
+#Removendo um elemento especifico
+lista.remove('Alladio')
+print(lista)
+
+#para inserir mais de um elemento
+lista.extend([3,4])
+print(lista)
+
+#diminuindo o tamanho da lista. 
+#[2:4] -> Siginifica: Exclua do 2 ao 4
+del lista[2:4]
+print(lista)
