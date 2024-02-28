81d1a35237f9b94ab5e7e6fd64fdc40cb31e7254
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-02-28 19:36:29 -0300

    Refact: some errors in SQL

--- teste_and_Questions/test01/06_SQL_Joins.sql
+++ teste_and_Questions/test01/06_SQL_Joins.sql
@@ -1,23 +1,23 @@
--- INNER JOIN - Selecionando todos os clientes com seus rescpectivos produtos
-SELECT cliente.nome, pedido.produto
+-- INNER JOIN - Selecionando todos os clientes com seus rescpectivos Pedidos
+SELECT cliente.nome, pedido.id
 FROM cliente
 INNER JOIN pedido
 ON cliente.id = pedido.cliente_id;
 
 -- LEFT JOIN - Selecionando todos os clientes mesmo se não tiver produtos
-SELECT cliente.nome, pedido.produto
+SELECT cliente.nome, pedido.id
 FROM cliente
 LEFT JOIN pedido
 ON cliente.id = pedido.cliente_id;
 
--- RIGHT JOIN - Selecionando todos os clientes mesmo se não tiver cliente (Ou não tenham sido pedidos por ninguém)
-SELECT cliente.nome, pedido.produto
+-- RIGHT JOIN - Selecionando todos os Pedidos mesmo se não tiver cliente (Ou não tenham sido pedidos por ninguém)
+SELECT cliente.nome, pedido.id
 FROM cliente
-LEFT JOIN pedido
+RIGHT JOIN pedido
 ON cliente.id = pedido.cliente_id;
 
--- FULL OUTER JOIN - Selecionando todos os clientes mesmo se não tiver cliente (Ou não tenham sido pedidos por ninguém)
-SELECT cliente.nome, pedido.produto
+-- FULL OUTER JOIN - Selecionando todos os clientes e pedidos mesmo se não tiver conrrespondência entre elas
+SELECT cliente.nome, pedido.id
 FROM cliente
-LEFT JOIN pedido
+FULL OUTER JOIN pedido
 ON cliente.id = pedido.cliente_id;
