bc3c7f28b8aa8f1de52b5677ff058ed50a2be96c
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-02-29 07:19:46 -0300

    Refact: some tests

--- teste_and_Questions/test01/08_Polymorphism.py
+++ teste_and_Questions/test01/08_Polymorphism.py
@@ -6,26 +6,32 @@ class Conta:
     def  __str__(self) -> str:
         return f"Conta {self.numero}: R${self.saldo: .2f}"
     
-    def depositar(self, valor: float):
-        saldo += valor
+    def depositar(self, valor: float) -> None:
+        self.saldo += valor
+        print(f"Saldo {self.saldo}")
+
+    def sacar(self, valor:float) -> None:
+        try:
+            if valor > self.saldo:
+                raise ValueError ("Saldo insuficiente - Saque")
+            self.saldo -= valor
+        except ValueError:
+            print(f"Erro ao sacar. Classe: {self.__class__.__name__}, Método: {self.sacar.__name__}")
+
+
+class contaCorrente(Conta):
+    def __init__(self, numero: str, saldo: float, limite) -> None:
+        super().__init__(numero, saldo)
+        self.limite = limite
 
     def sacar(self,valor:float):
         try:
             if valor > self.saldo:
                 raise ValueError ("Saldo insuficiente - Saque")
-            saldo -= valor
+            self.saldo -= valor
         except ValueError:
             print(f"Erro ao sacar. Classe: {self.__class__.__name__}, Método: {self.sacar.__name__}")
-    
-class contaCorrente(Conta):
-    def __init__(self, numero: str, saldo: float, limite) -> None:
-        super().__init__(numero, saldo)
-        self.limite = limite
-        
-    def sacar(self,valor:float):
-        try:
-            if valor > self.saldo:
-                raise ValueError ("Saldo insuficiente - Saque")
-            saldo -= valor
-        except ValueError:
-            print(f"Erro ao sacar. Classe: {self.__class__.__name__}, Método: {self.sacar.__name__}")
+
+contaNova = Conta("1234",55)
+contaNova.depositar(valor=100.00)
+contaNova.depositar(100.00)
