9639fc0b66365093ca01dfd1f18f8409507bd058
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-03-03 16:39:56 -0300

    Add: 11_SQL.sql (file)

---
+++ teste_and_Questions/02 - Factored test/11_SQL.sql
@@ -0,0 +1,5 @@
+--You have a table named Players with columns named player_id, player_name and player_team.
+--You need to display all display all the team that start with the letter A and end with the same letter
+-- (e.g, "ARGENTINA" or "AUSTRIA").
+
+LIKE 'A%A'
