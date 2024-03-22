cd375d36de79c0b3a8a0a1b08940cc10734a0e7c
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-03-22 17:20:27 -0300

    Feat: Add run function

--- teste_and_Questions/aula07_4_args_and_kwargs.py
+++ teste_and_Questions/aula07_4_args_and_kwargs.py
@@ -3,3 +3,11 @@ def exibir_texto(data, *args, **kwargs):
     meta_dados = "\n".join([f"{chave.title()}:{valor}" for chave, valor in kwargs.items()])
     mensagem = f"{data}\n\n{texto}\n\n{meta_dados}"
     print(mensagem)
+
+exibir_texto (
+    "Brasil 22/03/2024",
+    "Olá, bom dia, tudo bem?",
+    "Esse é um texte de args...",
+    "Masa vida é essa...",
+    autor="Alladio",
+    ano="2024")
